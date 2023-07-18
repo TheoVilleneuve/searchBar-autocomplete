0c9c95e50c7b1b62a4667255f1b109ced4202627
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-07-18 17:54:31 +0200

    mise en place du style de la page

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -9,10 +9,12 @@ import Select from "@mui/material/Select";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import Stack from "@mui/material/Stack";
+
 import Switch from "@mui/material/Switch";
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
+import { lightGreen } from "@mui/material/colors";
+import { alpha, styled } from "@mui/material/styles";
+
+import Button from "@mui/material/Button";
 
 export default function searchBar() {
   const [options, setOptions] = useState([]);
@@ -34,7 +36,7 @@ export default function searchBar() {
   const [adultCount, setAdultCount] = useState(1);
   const [youthCount, setYouthCount] = useState(0);
   const [seniorCount, setSeniorCount] = useState(0);
-  
+
   const handleChange = (event) => {
     setOneOrRound(event.target.value);
   };
@@ -93,50 +95,22 @@ export default function searchBar() {
   }, [topDestinations]);
 
   //Switch component
-  const AntSwitch = styled(Switch)(({ theme }) => ({
-    width: 28,
-    height: 16,
-    padding: 0,
-    display: "flex",
-    "&:active": {
-      "& .MuiSwitch-thumb": {
-        width: 15,
-      },
-      "& .MuiSwitch-switchBase.Mui-checked": {
-        transform: "translateX(9px)",
+  const GreenSwitch = styled(Switch)(({ theme }) => ({
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      color: lightGreen[600],
+      "&:hover": {
+        backgroundColor: alpha(
+          lightGreen[600],
+          theme.palette.action.hoverOpacity
+        ),
       },
     },
-    "& .MuiSwitch-switchBase": {
-      padding: 2,
-      "&.Mui-checked": {
-        transform: "translateX(12px)",
-        color: "#fff",
-        "& + .MuiSwitch-track": {
-          opacity: 1,
-          backgroundColor:
-            theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
-        },
-      },
-    },
-    "& .MuiSwitch-thumb": {
-      boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
-      width: 12,
-      height: 12,
-      borderRadius: 6,
-      transition: theme.transitions.create(["width"], {
-        duration: 200,
-      }),
-    },
-    "& .MuiSwitch-track": {
-      borderRadius: 16 / 2,
-      opacity: 1,
-      backgroundColor:
-        theme.palette.mode === "dark"
-          ? "rgba(255,255,255,.35)"
-          : "rgba(0,0,0,.25)",
-      boxSizing: "border-box",
+    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+      backgroundColor: lightGreen[600],
     },
   }));
+
+  const label = { inputProps: { "aria-label": "Color switch demo" } };
 
   return (
     <div className={styles.container}>
@@ -152,12 +126,8 @@ export default function searchBar() {
           </Select>
         </FormControl>
 
-        <FormControl
-          variant="standard"
-          sx={{ m: 1, minWidth: 200 }}
-        >
-        
-          <Select className={styles.count} value={'test'}>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+          <Select className={styles.count} value={"test"}>
             {/* Compteur de places cat.adulte */}
             <MenuItem value={adultCount} className={styles.counterContainer}>
               <div className={styles.categoryContainer}>
@@ -188,7 +158,7 @@ export default function searchBar() {
                 </button>
               </div>
             </MenuItem>
-{/* Compteur de places cat.jeune */}
+            {/* Compteur de places cat.jeune */}
             <MenuItem value={youthCount} className={styles.counterContainer}>
               <div className={styles.categoryContainer}>
                 <p className={styles.categoryTicket}>Youth</p>
@@ -262,7 +232,7 @@ export default function searchBar() {
           }}
           disablePortal
           options={options}
-          sx={{ width: 300 }}
+          sx={{ width: 280 }}
           inputValue={departure}
           onInputChange={(event, newInputValue) => {
             fetch(
@@ -290,7 +260,7 @@ export default function searchBar() {
           }}
           disablePortal
           options={arrivalOptions}
-          sx={{ width: 300 }}
+          sx={{ width: 280 }}
           inputValue={arrival}
           onInputChange={(event, newInputValue) => {
             fetch(
@@ -332,16 +302,23 @@ export default function searchBar() {
             />
           </LocalizationProvider>
         </div>
+        <Button
+          size="large"
+          variant="contained"
+          sx={{
+            backgroundColor: "#769259",
+            "&:hover": {
+              backgroundColor: "#325547",
+            },
+          }}
+        >
+          Search
+        </Button>
       </div>
 
       <div className={styles.switchContainer}>
-        <Stack direction="row" spacing={1} alignItems="center">
-          <AntSwitch
-            defaultChecked
-            inputProps={{ "aria-label": "ant design" }}
-          />
-          <Typography>Find my accommodation</Typography>
-        </Stack>
+        <GreenSwitch {...label} defaultChecked />
+        <p className={styles.switchText}>Find my accommodation</p>
       </div>
     </div>
   );
