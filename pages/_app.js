da5d2489b9570d31987d69f52ae9ce4d0b2253be
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-07-25 13:00:23 +0200

    title ok

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Autocomplete Search-bar</title>
       </Head>
       <Component {...pageProps} />
     </>
